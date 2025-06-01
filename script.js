4cbdf94f5401a75f30392ee414c8701c1e0d0edc
Tom Clark <tom.hm.clark@gmail.com>
2025-06-01 12:41:00 +0100

    Continued work on the navbar

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const navbar = document.getElementById('navbar')
+const overlay = document.querySelector(".body-overlay")
+const openBtn = document.querySelector(".open-sidebar-button");
+const closeBtn = document.querySelector(".close-sidebar-button");
 
 function openSidebar() {
   navbar.classList.add("show")
+  overlay.classList.add("active")
 }
 
 function closeSidebar() {
   navbar.classList.remove("show")
-
+  overlay.classList.remove("active")
 }
+
+openBtn.addEventListener("click", openSidebar);
+closeBtn.addEventListener("click", closeSidebar);
+overlay.addEventListener("click", closeSidebar);

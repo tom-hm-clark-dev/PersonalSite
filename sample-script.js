3b5ca34684199f5287818a7b8abb5cc82b68f5bf
Tom Clark <tom.hm.clark@gmail.com>
2025-08-07 15:54:20 +0100

    About to mess with the whole styling so this could get very wrong

--- sample-script.js
+++ sample-script.js
@@ -9,14 +9,28 @@ const sampleH2 = document.querySelector(".sample-h2");
 
 sampleBody.classList.remove("style-0", "style-1", "style-2", "style-3");
 sampleBody.classList.add("style-" + n);
-console.log("Style changed to: " + n);
 if (n == 0) {   sampleH1.innerHTML = "Your brand,";  }
 if (n == 1) {   sampleH1.innerHTML = "Your brand,";
     sampleH2.innerHTML = "Your way.";
   }
 if (n == 2) {   sampleH1.innerHTML = "Creative, responsive website designs";  
     sampleH2.innerHTML = "All built to your specification.";   }
-if (n == 3) {   sampleH1.innerHTML = "Get started today";  }
+if (n == 3) {   sampleH1.innerHTML = "Get started today";  
+    sampleH2.innerHTML = "The next step in your business waits";   } 
+}
+
+// TEMP FUNCTION
+function toggleSampleOverlay() {
+  const sampleOverlay = document.querySelector(".sample-overlay");
+  /* if (!sampleOverlay.classList.contains("active")) {
+    sampleOverlay.classList.add("active");
+  }
+  else {
+    sampleOverlay.classList.remove("active");
+  } */
+ sampleOverlay.addEventListener("animationend", () => {
+  sampleOverlay.classList.add("fade-out");
+});
 }
 
 function plusStyle(n){
@@ -26,4 +40,5 @@ function plusStyle(n){
   if (styleIndex < 0) styleIndex = 2;
 
   changeStyle(styleIndex);
+  toggleSampleOverlay();
 }
